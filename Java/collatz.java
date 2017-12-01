33e6c31b3645c1766703813e2eb73a93049350ec
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-01 17:07:31 -0500

    Java Runs; Not Checked

--- Java/collatz.java
+++ Java/collatz.java
@@ -1,17 +1,16 @@
 import java.math.BigInteger;
-import java.util;
 
-public class collatz implements Comparator<collatz> {
+public class collatz {
 
 	private BigInteger start;
-	private BigInteger length;
+	private int length;
 
 	public collatz(){
 		this.start = BigInteger.ZERO;
-		this.length = BigInteger.ZERO;
+		this.length = 0;
 	}
 
-	public collatz(BigInteger start, BigInteger length){
+	public collatz(BigInteger start, int length){
 		this.start = start;
 		this.length = length;
 	}
@@ -20,7 +19,7 @@ public class collatz implements Comparator<collatz> {
 		this.start = start;
 	}
 
-	public void setLength(BigInteger length){
+	public void setLength(int length){
 		this.length = length;
 	} 
 
@@ -28,19 +27,7 @@ public class collatz implements Comparator<collatz> {
 		return this.start;
 	}
 
-	public BigInteger getLength(){
+	public int getLength(){
 		return this.length;
-	}
-
-	public int compareStart(collatz a, collatz b){
-		if( a < b ){
-			return -1;
-		}
-		else if( a == b ){
-			return 0;
-		}
-		else{
-			return 1;
-		}
 	}
 }
--- Java/runCollatz.java
+++ Java/runCollatz.java
@@ -1,32 +1,71 @@
-import java.math.BigInteger;
-import java.util.Vector;
+import java.math.*;
+import java.util.*;
+import java.lang.Object;
 
 public class runCollatz{
 
 	public static void main(String[] args){
 
 		// The height of the sequence start values
-		BigInteger end = 10000;
+		BigInteger end = new BigInteger("10000");
 		System.out.println("Finding 10 longest sequences between 1 and " + end);
 
 		// Holds all Collatz Objects found between 2 and the end
-		Vector all = new Vector(0, 1);		
+		ArrayList<collatz> all = new ArrayList<collatz>();		
 		// Holds the elements of the current collatz sequence
-		Vector sequence = new Vector(0,1);
+		ArrayList<BigInteger> sequence = new ArrayList<BigInteger>();
 
 		// Holds the size of the current collatz sequnce
 		int size;
+		BigInteger temp;
+		for( int i = 2; i < end.intValue() ; i++ ){
 
-		for( BigInteger i = 2; i < end; i++ ){
-
-			sequence = collatz(i);
+			temp = BigInteger.valueOf(i);
+		
+			sequence = Collatz(temp);
 			size = sequence.size()-1;
 
-			all.addElement(new Collatz(i, size));
+			all.add(new collatz(temp, size));
 		
 			sequence.clear();		
 		}
 
-		
+		// Sort in ascending order by sequence length
+		Collections.sort(all, new sortByLength());	
+
+		int top = 1;
+		for( int j = all.size()-1; j > 1; j-- ){
+
+			if( all.get(j).getLength() > all.get(j-1).getLength() ){
+				System.out.println( (all.get(j).getStart()).toString() + " " + all.get(j).getLength() );
+				top++;
+			}
+			if( top > 10 ){
+				break;
+			}
+		}
 	}
+
+	public static ArrayList<BigInteger> Collatz( BigInteger start ){
+		
+		ArrayList<BigInteger> sequence = new ArrayList<BigInteger>();
+		sequence.add( start );
+
+		BigInteger zero, one, two, three;
+		one = BigInteger.valueOf(1); two = BigInteger.valueOf(2);
+		three = BigInteger.valueOf(3); zero = BigInteger.valueOf(0);
+
+		while( !start.equals( one ) ){
+
+			if( start.mod( two ) == zero ){
+				start = start.divide( two );
+			}
+			else{
+				start = (start.multiply( three )).add( one );
+			}
+			sequence.add( start );
+		}
+		return sequence;
+	}
+
 }
