3e8a368ab697faad083cbbbfa9a1e8281aeaade9
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-01 23:11:38 -0500

    Lisp Unsuccessful: Resizable Arrays Causing Difficulty

--- Lisp/collatz.lisp
+++ Lisp/collatz.lisp
@@ -13,17 +13,36 @@
 (defvar seqSize)
 (defvar sequenz)
 (defvar allSeq)
+(defvar sequenzSize)
+(defvar allSize)
+(defvar i)
 
 (setf sequenz (make-array 1 :element-type 'integer :adjustable t))
+(setf sequenzSize 0)
 (setf allSeq (make-array 1 :element-type 'collatz :adjustable t))
+(setf allSize 1)
 
 (defun newCollatz (start size)
 	(make-instance 'collatz :start start :size size))
 
 (defun getCollatz (start)
 	(setf (aref sequenz 0 ) start)
-	(loop (while (start != 1))
-		(if ((% start 2) == 0)
+	(loop while (not(eq start 1)) do
+		(if (eq (mod start 2) 0)
 			(setf start (/ start 2))
 			(setf start (+ (* 3 start) 1)))
 		(vector-push-extend 1 sequenz)
+		(setf sequenzSize (+ sequenzSize 1))
+		(setf (aref sequenz sequenzSize) start ))
+	(return-from getCollatz sequenz))
+
+(setf endVal 10000)
+(setf i 2)
+
+(dotimes (i endVal)
+	(setf sequenz getCollatz(i))
+	(defvar c newCollatz(i size))
+	(vector-push-extend 1 allSeq)
+	(setf allSize (+ allSize 1))
+	(setf (aref allSeq allSize) c)
+)
