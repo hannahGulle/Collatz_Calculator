5e8c1dc96bdf08540cb60d9912ac9bd84b8e5019
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-04 18:13:48 -0500

    List Unrecursive Successful

--- Lisp/collatz.lisp
+++ Lisp/collatz.lisp
@@ -1,48 +1,79 @@
 #!/usr/bin/sbcl --script
 
-(defclass collatz ()
-	((start :accessor start
-		:initform 0
-		:initarg :start)
-	(size 	:accessor size
-		:initform 0
-		:initarg :size)))
+(defstruct collatz 
+	(start 0)
+	(size 0))
 
 (defvar top)
 (defvar endVal)
-(defvar seqSize)
-(defvar sequenz)
 (defvar allSeq)
-(defvar sequenzSize)
-(defvar allSize)
+(defvar seqSize)
 (defvar i)
+(defvar j)
+(defvar k)
+(defvar temp)
 
-(setf sequenz (make-array 1 :element-type 'integer :adjustable t))
-(setf sequenzSize 0)
-(setf allSeq (make-array 1 :element-type 'collatz :adjustable t))
-(setf allSize 1)
+(format t "What's the Hightest Value?~%")
+(setf endVal (read))
+(setf allSeq (make-array endVal))
 
-(defun newCollatz (start size)
-	(make-instance 'collatz :start start :size size))
+(setf seqSize 0)
 
-(defun getCollatz (start)
-	(setf (aref sequenz 0 ) start)
+; Returns the size of the collatz sequence
+(defun getCollatz (start seqSize)
+	(setf seqSize 0)
 	(loop while (not(eq start 1)) do
 		(if (eq (mod start 2) 0)
 			(setf start (/ start 2))
-			(setf start (+ (* 3 start) 1)))
-		(vector-push-extend 1 sequenz)
-		(setf sequenzSize (+ sequenzSize 1))
-		(setf (aref sequenz sequenzSize) start ))
-	(return-from getCollatz sequenz))
+			(setf start (+ (* 3 start) 1))
+		)
+		(setf seqSize (+ seqSize 1 ))
+	)		
+	(return-from getCollatz seqSize))
 
-(setf endVal 10000)
+; Find Collatz Sequence for Each number from 2 to the Highest End Value (endVal)
 (setf i 2)
+(loop
+	(setf seqSize (getCollatz i seqSize))
+	(setf (aref allSeq (- i 1)) (make-collatz :start i :size seqSize))
+	(setf i (+ i 1))
+	(when (> i endVal) (return))
+)
 
-(dotimes (i endVal)
-	(setf sequenz getCollatz(i))
-	(defvar c newCollatz(i size))
-	(vector-push-extend 1 allSeq)
-	(setf allSize (+ allSize 1))
-	(setf (aref allSeq allSize) c)
+; Sort Array of Collatz Structs
+(setf i 1)
+(loop
+	(setf j i)
+	(setf k (+ i 1))
+	(loop		
+		(if (< (collatz-size (aref allSeq k)) (collatz-size (aref allSeq j)))
+			(setf j k))		
+		(setf k (+ k 1))
+		(when (>= k endVal) (return))
+	)
+
+	(if (not (eq j i))
+		(progn
+			(setf temp (aref allSeq i))
+			(setf (aref allSeq i) (aref allSeq j ))
+			(setf (aref allSeq j) temp)
+		)
+	)
+	(setf i (+ i 1))
+	(when (>= i (- endVal 1)) (return))
+)
+
+(setf top 1)
+(setf i (- endVal 1))
+(loop
+	(if (> (collatz-size (aref allSeq i)) (collatz-size (aref allSeq (- i 1))))
+		(progn
+			(format t "~D ~D ~%" (collatz-start (aref allSeq i)) (collatz-size (aref allSeq i)))
+			(setf top (+ top 1))
+		)
+	)
+	(when (> top 10) (return))
+
+	(setf i (- i 1))
+	(when (<= i 2) (return))
 )
