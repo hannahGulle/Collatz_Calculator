963ad176f6d677ac80f8fb95f5e1e14ea8e69f1f
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-04 18:21:22 -0500

    List Recursion Successful

---
+++ Lisp/Recursed/collatzRec.lisp
@@ -0,0 +1,92 @@
+#!/usr/bin/sbcl --script
+
+(defstruct collatz 
+	(start 0)
+	(size 0))
+
+(defvar top)
+(defvar endVal)
+(defvar allSeq)
+(defvar seqSize)
+(defvar i)
+(defvar j)
+(defvar k)
+(defvar temp)
+(defvar start)
+
+(format t "What's the Hightest Value?~%")
+(setf endVal (read))
+(setf allSeq (make-array endVal))
+
+(setf seqSize 0)
+
+; Returns the size of the collatz sequence
+(defun getCollatz (start seqSize)
+
+	(if (eq start 1)
+		(return-from getCollatz seqSize)
+		(progn; Else start != 1
+			(if (eq (mod start 2) 0)
+				(progn
+					(setf start (/ start 2))
+					(setf seqSize (+ seqSize 1))
+					(return-from getCollatz (getCollatz start seqSize))
+				)
+				(progn
+					(setf start (+ (* start 3) 1))
+					(setf seqSize (+ seqSize 1))
+					(return-from getCollatz (getCollatz start seqSize))
+				)
+			)
+		)
+	)
+)	
+
+; Find Collatz Sequence for Each number from 2 to the Highest End Value (endVal)
+(setf i 2)
+(loop
+	(setf seqSize 0)
+	(setf start i)
+	(setf seqSize (getCollatz start seqSize))
+	(setf (aref allSeq (- i 1)) (make-collatz :start i :size seqSize))
+	(setf i (+ i 1))
+	(when (> i endVal) (return))
+)
+
+; Sort Array of Collatz Structs
+(setf i 1)
+(loop
+	(setf j i)
+	(setf k (+ i 1))
+	(loop		
+		(if (< (collatz-size (aref allSeq k)) (collatz-size (aref allSeq j)))
+			(setf j k))		
+		(setf k (+ k 1))
+		(when (>= k endVal) (return))
+	)
+
+	(if (not (eq j i))
+		(progn
+			(setf temp (aref allSeq i))
+			(setf (aref allSeq i) (aref allSeq j ))
+			(setf (aref allSeq j) temp)
+		)
+	)
+	(setf i (+ i 1))
+	(when (>= i (- endVal 1)) (return))
+)
+
+(setf top 1)
+(setf i (- endVal 1))
+(loop
+	(if (> (collatz-size (aref allSeq i)) (collatz-size (aref allSeq (- i 1))))
+		(progn
+			(format t "~D ~D ~%" (collatz-start (aref allSeq i)) (collatz-size (aref allSeq i)))
+			(setf top (+ top 1))
+		)
+	)
+	(when (> top 10) (return))
+
+	(setf i (- i 1))
+	(when (<= i 2) (return))
+)
