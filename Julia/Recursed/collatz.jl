a4236d6ffa6babf0c57740e15d0a112db72d3391
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-05 16:23:12 -0500

    JULIA COMPLETE

--- Julia/Recursed/collatz.jl
+++ Julia/Recursed/collatz.jl
@@ -1,10 +1,15 @@
+# Hannah gulle
+# Program computes the top 10 collatz seqeunces for 
+# a given starting range
 
+# collatz type holds sequence start and sequence size
 type Collatz
 	start	:: Int
 	size	:: Int
 end;
 
-
+# computes the collatz seqeucne size for a given
+# starting integer recursively
 function getCollatz(seq, start::Int)
 
 	if start == 1 
@@ -18,14 +23,27 @@ function getCollatz(seq, start::Int)
 	end
 end
 
+# returns the size of a given collatz object
 function getSize( c::Collatz)
 	return c.size
 end
 
+# returns the start of a given collatz object
+function getStart( c::Collatz )
+	return c.start
+end
 
-endVal = 10000
+# request the range upper bound from the user
+println("Input the Highest Starting Integer")
+endVal = readline()
+endVal = parse(Int, endVal)
+
+# create an array of collatz types to hold all
+# collatz sequence size and start values
 allseq = Collatz[]
 
+# iterate through the range to find the collatz
+# seqeunce size
 for i= 2:endVal
 
 	sequenz = [i]
@@ -37,16 +55,35 @@ for i= 2:endVal
 	empty!(sequenz)
 end
 
+println("")
+println("Top 10 Starting Values After Sorting by Length")
+
+# sort the array of collatz types by seqeunce size
 sort!( allseq, by=getSize)
 
+# array to hold top 10 values
 top = 1
+topTen = Collatz[]
 
+# output top 10 values sorted by size
 for i= countnz(allseq):-1:1
 	if allseq[i].size > allseq[i-1].size
+		push!( topTen, allseq[i] )
 		println( allseq[i].start, " ", allseq[i].size)
 		top = top+1
 	end
 	if top > 10
 		break
 	end
+end
+
+println("")
+println("Top 10 Starting Values After Sorting by Start")
+
+# sort the top 10 values by start
+sort!( topTen, by=getStart)
+
+# output top 10 after sorting by start
+for i=10:-1:1
+	println( topTen[i].start, " ", topTen[i].size)
 end
