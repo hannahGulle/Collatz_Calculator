9aebbc225e1d852b347a66f6c2946d6708d226f8
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-05 17:35:53 -0500

    PYTHON COMPLETE

--- Python/Recursed/collatz.py
+++ Python/Recursed/collatz.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+# Hannah Gulle
+# Program computes the top 10 starting numbers whose
+# collatz sequence is largest within the user
+# specified length
 
+# collatz composite structure holds the sequence
+# start and length
 class collatz:
 	def __init__(self, Start, Length):
 		self.Start = Start
 		self.Length = Length
 
-
+# retruns the collatz sequence for a given starting value
+# recursively
 def getCollatz(sequence, start):
 
 	if start == 1:
@@ -17,9 +24,17 @@ def getCollatz(sequence, start):
 		sequence.append( (3*start)+1 )
 		return getCollatz( sequence, (3*start)+1 )
 
+# retrieve the highest starting integer from the user
+end = int(input("Input the Highest Starting Integer: "))
 
-end = int(10000)
+# array to hold all collatz structures for the range
 allseq = []
+
+# array to hold all collatz structures in the topTen
+topTen = []
+
+# find the collatz sequence structure values for
+# each starting value within the range
 for i in range(2,end):
 
 	sequence = [i]
@@ -31,17 +46,29 @@ for i in range(2,end):
 
 	sequence[:] = []
 
+print("\nTop 10 Starting Integers After Sorting By Length\n")
+# sort all collatz structures by sequence length
 allseq.sort(key=lambda collatz: collatz.Length)
 
+# output the top 10 by sequence length
+# and add to the topTen array
 top = int(1);
 for i in range( len(allseq)-1 , 1 , -1):
 	if allseq[i].Length > allseq[i-1].Length:
+		topTen.append( allseq[i] )
 		print( allseq[i].Start, allseq[i].Length )
 		top += 1
 	if top > 10 :
 		break
 
+# sort the top 10 collatz by sequence start
+topTen.sort(key=lambda collatz: collatz.Start)
 
+print("\nTop 10 Starting Integers After Sorting by Start")
+
+# output the top 10 by sequence start
+for i in range( len(topTen)-1, -1, -1):
+	print( topTen[i].Start, topTen[i].Length)
 
 
 
