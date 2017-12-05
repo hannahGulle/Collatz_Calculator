db5e2ec8cda842cf95dde63ac831ac0b72c2810c
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-04 20:45:47 -0500

    C# Arb Incomplete: Needs User Input

--- C#/collatz_arb.cs
+++ C#/collatz_arb.cs
@@ -1,4 +1,6 @@
-
+// Hannah Gulle
+// Computes the top 10 collatz sequences from 2 to a given
+// starting integer using arbitrary precision
 
 using System;
 using System.Collections.Generic;
@@ -8,45 +10,62 @@ namespace CollatzSequence{
 
 	class collatz{
 
+		// Arbitrary Precision Valued Start and Length of the 
+		// Given Collatz Sequence and Collatz Object
 		private BigInteger start;
 		private BigInteger length;
 
+		// Empty Constructor
 		public collatz(){
 			this.start = BigInteger.Zero;
 			this.length = BigInteger.Zero;
 		}
 
+		// Constructor
 		public collatz( BigInteger start, BigInteger length ){
 			this.start = start;
 			this.length = length;
 		}
 
+		// Sets the Arbitrary Precision Start Value
 		public void setStart( BigInteger start ){
 			this.start = start;
 		}
 
+		// Sets the Arbitrary Precision Length Value
 		public void setLength( BigInteger length ){
 			this.length = length;
 		}
 
+		// Retrieves the Arbitrary Precision Start Value
 		public BigInteger getStart(){
 			return this.start;
 		}
 
+		// Retrieves the Arbitrary Precision Length Value
 		public BigInteger getLength(){
 			return this.length;
 		}
 	}
 
+	// The Main Program
 	class runCollatz{
 
 		static void Main(){
 
+			// TODO: User Input
+
 			BigInteger end = 10000;
 
+			// List of All collatz Objects for the given Starting Value
+			// Range
 			List<collatz> all = new List<collatz>();
+			// List of all sequence values for a given starting value
 			List<BigInteger> sequence = new List<BigInteger>();
 
+			// Retrieves and Constructs a collatz object for each starting
+			// value from 2 to the highest starting value and inputs the 
+			// sequence size into the object
 			BigInteger size;
 			for(BigInteger i = 2; i < end; i++){
 
@@ -56,15 +75,23 @@ namespace CollatzSequence{
 				all.Add( new collatz( i, size ));
 				sequence.Clear();
 			}	
-		
+			
+			Console.WriteLine("Top 10 Starting Values After Sorting by Length");
+			
+			// Sorts Collatz Object list by Length
 			all.Sort(delegate( collatz c1, collatz c2 ){
 				return c1.getLength().CompareTo( c2.getLength() );
 			});
 
+			// List of top ten collatz objects by length
+			List<collatz> topTen = new List<collatz>();
+
+			// Determines the top 10 values of the list of all collatz objects
 			int top = 1;
 			for( int i = all.Count-1; i > 1; i--){
 
 				if( all[i].getLength() > all[i-1].getLength() ){
+					topTen.Add( all[i] );
 					Console.WriteLine( "{0} {1}", all[i].getStart(), all[i].getLength());
 					top++;
 				}
@@ -72,8 +99,21 @@ namespace CollatzSequence{
 					break;
 				}
 			}
+		
+			Console.WriteLine("\n Top 10 Starting Values After Sorting by Start");
+	
+			// Sorts collatz object list by start
+			topTen.Sort(delegate( collatz c1, collatz c2 ){
+				return c1.getStart().CompareTo( c2.getStart() );
+			});
+
+			// Outputs the top 10 starting values after sorting by start
+			for( int i = 9; i > 0; i-- ){
+				Console.WriteLine("{0} {1}", topTen[i].getStart(), topTen[i].getLength() );
+			}
 		}
 	
+		// Computes the colltz sequence for a given starting position
 		public static List<BigInteger> Collatz( BigInteger start ){
 
 			List<BigInteger> sequence = new List<BigInteger>();
