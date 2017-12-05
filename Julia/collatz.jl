d7d41c824f646573bb050cb3f2a2991f0894aaf3
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-04 23:23:03 -0500

    Julia NonArb NonRec COMPLETE

--- Julia/collatz.jl
+++ Julia/collatz.jl
@@ -1,10 +1,16 @@
+# Hannah Gulle
+# Program computes the top 10 collatz sequences for
+# a given starting range 
 
+# Collatz Type holds sequence start and sequence size
 type Collatz
 	start	:: Int
 	size	:: Int
 end;
 
 
+# computes the collatz sequence size for a given
+# starting integer
 function getCollatz(start::Int)
 	seq = [start];
 
@@ -20,14 +26,28 @@ function getCollatz(start::Int)
 	return seq
 end
 
+# returns the size of a given collatz object
 function getSize( c::Collatz)
 	return c.size
 end
 
 
-endVal = 10000
+# returns the start of a given collatz object
+function getStart( c::Collatz)
+	return c.start
+end
+
+# Request the range upper bound from  the user
+println("Input the Highest Starting Integer")
+endVal = readline()
+endVal = parse(Int, endVal)
+
+# create an array of collatz types to hold all
+# collatz sequence size and start values
 allseq = Collatz[]
 
+# iterate through the range to find the
+# collatz sequence size
 for i= 2:endVal
 
 	sequenz = getCollatz(i)
@@ -38,16 +58,35 @@ for i= 2:endVal
 	empty!(sequenz)
 end
 
+println("")
+println("Top 10 Starting Values After Sorting By Length")
+
+# sort the array of collatz objects
+# by sequence size
 sort!( allseq, by=getSize)
 
+# array to hold top 10 values
 top = 1
+topTen = Collatz[]
 
+# output top 10 values sorted by size
 for i= countnz(allseq):-1:1
 	if allseq[i].size > allseq[i-1].size
+		push!(topTen, allseq[i] )
 		println( allseq[i].start, " ", allseq[i].size)
 		top = top+1
 	end
 	if top > 10
 		break
 	end
+end
+
+println("")
+println("Top 10 Starting Values After Sorting By Start")
+
+# sort the top 10 values by start
+sort!( topTen, by=getStart )
+
+for i=10:-1:1
+	println( topTen[i].start, " ", topTen[i].size)
 end
