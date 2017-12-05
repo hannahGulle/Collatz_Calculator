087c1f0376b7d7ed6bdd74c91e8d7a659b54deb7
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-05 17:31:38 -0500

    Python Arb COMPLETE

--- Python/Arbitrary/collatz_arb.py
+++ Python/Arbitrary/collatz_arb.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+# Hannah Gulle
+# Program computes the top 10 starting numbers whose
+# collatz sequence is largest within the user
+# specified length
 
+# collatz composite structure holds the sequence
+# start and length
 class collatz:
 	def __init__(self, Start, Length):
 		self.Start = Start
 		self.Length = Length
 
-
+# returns the collatz sequence for a given starting value
+# non recursively with arbitrary precision
 def getCollatz(start):
 	sequence = [start]
 
@@ -17,9 +24,17 @@ def getCollatz(start):
 		sequence.append(start)
 	return sequence
 
+# retrieve the highest starting integer from the user
+end = long(input("Input the Highest Starting Integer: "))
 
-end = long(10000)
+# array to hold all collatz structures for the range
 allseq = []
+
+# array to hold all collatz structures in the top 10 range
+topTen = []
+
+# find the collatz sequence structure values for 
+# each starting value within the range
 for i in range(2,end):
 	sequence = getCollatz(i)
 	size = len(sequence)-1
@@ -29,12 +44,27 @@ for i in range(2,end):
 
 	sequence[:] = []
 
+print("\nTop 10 Starting Integers After Sorting by Length")
+# sort all collatz structures by sequence Length
 allseq.sort(key=lambda collatz: collatz.Length)
 
+# output the top 10 by sequence length
+# and add to the topTen array
 top = int(1);
 for i in range( len(allseq)-1 , 1 , -1):
 	if allseq[i].Length > allseq[i-1].Length:
+		topTen.append( allseq[i] )
 		print "%d %d" % (allseq[i].Start, allseq[i].Length)
 		top += 1
 	if top > 10 :
 		break
+
+# sort the top 10 collatz by sequence start
+topTen.sort(key=lambda collatz: collatz.Start)
+
+print("\nTop 10 Starting Integers After Sorting by Start")
+
+# output the top 10 by sequence start
+for i in range( len(topTen)-1, -1, -1):
+	print( "%d %d" % (topTen[i].Start, topTen[i].Length))
+
