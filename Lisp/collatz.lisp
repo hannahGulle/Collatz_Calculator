92fd0177e65e6f037068bddd441836065051f631
Hannah Gulle <gulle_hc@cobra.cs.mercer.edu>
2017-12-05 16:50:05 -0500

    Lisp NonRec COMPLETE

--- Lisp/collatz.lisp
+++ Lisp/collatz.lisp
@@ -1,23 +1,35 @@
 #!/usr/bin/sbcl --script
+; Hannah Gulle
+; Program computes the top 10 starting values with the given
+; user input range for collatz sequences
 
+; collatz composite structure holds the sequence
+; start and size values
 (defstruct collatz 
 	(start 0)
 	(size 0))
 
-(defvar top)
-(defvar endVal)
-(defvar allSeq)
-(defvar seqSize)
-(defvar i)
-(defvar j)
-(defvar k)
-(defvar temp)
+(defvar top)		; number of top integer values
+(defvar endVal)		; highest starting integer
+(defvar allSeq)		; array of all collatz structures
+(defvar seqSize)	; array of all collatz values in a single sequence
+(defvar topTen)		; array of top 10 collatz structures
+(defvar i)		; iterative variable
+(defvar j)		; iterative variable
+(defvar k)		; iterative variable
+(defvar temp)		; sorting temporary variable
 
+
+; Retrieve the highest starting integer from the user
 (format t "What's the Hightest Value?~%")
 (setf endVal (read))
 (setf allSeq (make-array endVal))
 
+; set initial sequence size to zero
 (setf seqSize 0)
+
+; make the topTen array to hold 10 elements
+(setf topTen (make-array 11))
 
 ; Returns the size of the collatz sequence
 (defun getCollatz (start seqSize)
@@ -40,7 +52,7 @@
 	(when (> i endVal) (return))
 )
 
-; Sort Array of Collatz Structs
+; Sort Array of Collatz Structs by Size
 (setf i 1)
 (loop
 	(setf j i)
@@ -63,11 +75,16 @@
 	(when (>= i (- endVal 1)) (return))
 )
 
+(format t "~%Top 10 Starting Values After Sorting by Size~%")
+
+; Output top 10 starting values after sorting by sequence size
 (setf top 1)
 (setf i (- endVal 1))
 (loop
 	(if (> (collatz-size (aref allSeq i)) (collatz-size (aref allSeq (- i 1))))
 		(progn
+			; add each top 10 object to the topTen array
+			(setf (aref topTen top) (aref allSeq i))
 			(format t "~D ~D ~%" (collatz-start (aref allSeq i)) (collatz-size (aref allSeq i)))
 			(setf top (+ top 1))
 		)
@@ -76,4 +93,37 @@
 
 	(setf i (- i 1))
 	(when (<= i 2) (return))
+)
+
+
+; Sort topTen Array of Collatz Structs by start1
+(setf i 1)
+(loop
+	(setf j i)
+	(setf k (+ i 1))
+	(loop		
+		(if (< (collatz-start (aref topTen k)) (collatz-start (aref topTen j)))
+			(setf j k))		
+		(setf k (+ k 1))
+		(when (>= k 11) (return))
+	)
+
+	(if (not (eq j i))
+		(progn
+			(setf temp (aref topTen i))
+			(setf (aref topTen i) (aref topTen j ))
+			(setf (aref topTen j) temp)
+		)
+	)
+	(setf i (+ i 1))
+	(when (>= i 10) (return))
+)
+
+; output top 10 starting values after sorting by start
+(format t "~%Top 10 Starting Values After Sorting by Start~%")
+(setf i 10)
+(loop
+	(format t "~D ~D ~%" (collatz-start (aref topTen i)) (collatz-size (aref topTen i)))
+	(setf i (- i 1))
+	(when (<= i 0) (return))
 )
